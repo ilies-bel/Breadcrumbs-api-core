7dee914156ae2665a6bc8a9868ee98e53c15d7a4
ilies-bel <beldjilali.ilies@gmail.com>
2021-02-21 15:39:17 +0100

    Cleanup

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
-class SessionsController < Devise::SessionsController
+class Users::SessionsController < Devise::SessionsController
+
   def create
     user = User.find_by_email(sign_in_params[:email])
 
@@ -10,4 +11,5 @@ class SessionsController < Devise::SessionsController
       json_response(json: { errors: { 'email or password' => ['is invalid'] } }, status: :unprocessable_entity)
     end
   end
+
 end
